c7c27b036cf03b272e0be022387c8c8c61b7c41a
Celine Young <celine.kyoung@gmail.com>
2018-08-20 18:38:22 +1200

    Retrieves list of photos from firebase

--- photon/src/pages/profile/profile.ts
+++ photon/src/pages/profile/profile.ts
@@ -1,6 +1,6 @@
-import { PhotoPage } from './../photo/photo';
-import { OptionsPage } from './../options/options';
-import { CustomiseProfilePage } from './../customise-profile/customise-profile';
+import { PhotoPage } from "./../photo/photo";
+import { OptionsPage } from "./../options/options";
+import { CustomiseProfilePage } from "./../customise-profile/customise-profile";
 import { AngularFirestore } from "angularfire2/firestore";
 import { AuthService } from "./../../services/auth.service";
 import { User } from "./../../app/models/user.interface";
@@ -20,7 +20,8 @@ export class ProfilePage {
     email: "",
     biography: ""
   };
-  public imageSource: string = "https://instagram.fakl1-2.fna.fbcdn.net/vp/36bedd66b5fa8b8f6bf81650823a72f0/5BFC9C56/t51.2885-19/s150x150/38096749_208075379863871_8613051600635691008_n.jpg";
+  public imageSource: string =
+    "https://instagram.fakl1-2.fna.fbcdn.net/vp/36bedd66b5fa8b8f6bf81650823a72f0/5BFC9C56/t51.2885-19/s150x150/38096749_208075379863871_8613051600635691008_n.jpg";
   public postedPhotos: Photo[] = [];
 
   constructor(
@@ -44,15 +45,24 @@ export class ProfilePage {
   }
 
   getPostedPhotos() {
-    /*
     this.firestore
       .collection("users")
       .doc(this.auth.getUID())
       .collection("photos")
       .valueChanges()
       .subscribe(photos => {
-        this.photos = photos;
-      });*/
+        photos.forEach((photoData: Photo) => {
+          const photo: Photo = {
+            description: photoData.description,
+            locationDescription: photoData.locationDescription,
+            coordinates: photoData.coordinates,
+            timestamp: photoData.timestamp,
+            url: photoData.url
+          };
+          this.postedPhotos[this.postedPhotos.length] = photo;
+          console.log(photo.description);
+        });
+      });
   }
 
   optionsButtonClicked() {
@@ -63,7 +73,7 @@ export class ProfilePage {
     this.navCtrl.push(PhotoPage, {
       userID: userID,
       photoID: photoID,
-      source: 'profile'
+      source: "profile"
     });
   }
 
